c0ce5d5adf5be91525c4dfaf868ce039e758c4dd
Giampiero <giampierof2011@hotmail.it>
2024-04-09 12:11:50 +0200

    ok es1

---
+++ Create a sum script/main.js
@@ -0,0 +1,3 @@
+const sumScript=require('./sumScript');
+
+console.log(sumScript()); 
--- Create a sum script/sumScript.js
+++ Create a sum script/sumScript.js
@@ -1,1 +1,6 @@
 //Create a script that outputs the total of 5 + 6 when you run it with Node.js
+function sumScript() {
+    return (5+6)
+}
+
+module.exports = sumScript;
